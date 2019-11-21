b8097be784657eac1d05fc8e2346ae865405492c
Christos Paschalidis <paschalidi@outlook.com>
2019-11-21 14:34:34 +0100

    cleans CRA from boilerplate
    cleans CRA from boilerplate

--- packages/feedback-webbapp/src/index.js
+++ packages/feedback-webbapp/src/index.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {GlobalStyles, Fonts} from "@simple/components";
+import React from "react";
+import ReactDOM from "react-dom";
+import { GlobalStyles, Fonts } from "@simple/components";
+import Routes from "./Routes";
 
 ReactDOM.render(
   <div>
-    <Fonts/>
-    <GlobalStyles/>
-    <App />
-  </div>
-  , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <Fonts />
+    <GlobalStyles />
+    <Routes />
+  </div>,
+  document.getElementById("root")
+);
