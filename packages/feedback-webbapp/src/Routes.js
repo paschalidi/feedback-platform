b416c227bd50825d70c87ca2a4035e9ff12331a6
Christos Paschalidis <paschalidi@outlook.com>
2019-11-21 14:36:45 +0100

    implements simple routing of the app

---
+++ packages/feedback-webbapp/src/Routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavigationBar from "./NavigationBar";
+
+const menuItems = {
+  0: { title: "Share feedback", route: "/share-feedback" },
+  1: { title: "My feedback", route: "/my-feedback" }
+};
+function Routes() {
+  return (
+    <React.Fragment>
+      <Router>
+        <NavigationBar menuItems={menuItems} />
+        <Switch>
+          <Route path="/share-feedback">
+            {() => <div>share-feedback</div>}
+          </Route>
+          <Route path="/my-feedback">{() => <div>my-feedback</div>}</Route>
+          <Route path="/">{() => <div>home</div>}</Route>
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default Routes;
