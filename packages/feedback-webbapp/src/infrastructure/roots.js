ef17e5b33639c2a71e9019400e17df66933512df
Christos Paschalidis <paschalidi@outlook.com>
2019-11-24 18:26:21 +0100

    adds Questionnaire functionality

--- packages/feedback-webbapp/src/infrastructure/roots.js
+++ packages/feedback-webbapp/src/infrastructure/roots.js
@@ -1,12 +1,19 @@
 import { combineReducers } from "redux";
 import { combineEpics } from "redux-observable";
 import { toMeFeedbackReducer } from "../toMeFeedback/reducer";
-import { byMeFeedbackReducer } from "../byMeFeedback/reducer";
 import { toMeFeedbackEpic } from "../toMeFeedback/epic";
+import { byMeFeedbackReducer } from "../byMeFeedback/reducer";
 import { byMeFeedbackEpic } from "../byMeFeedback/epic";
+import { shareFeedbackReducer } from "../questionnaire/reducer";
+import { shareFeedbackEpic } from "../questionnaire/epic";
 
-export const rootEpic = combineEpics(toMeFeedbackEpic, byMeFeedbackEpic);
+export const rootEpic = combineEpics(
+  toMeFeedbackEpic,
+  byMeFeedbackEpic,
+  shareFeedbackEpic
+);
 export const rootReducer = combineReducers({
   toMeFeedbackReducer,
-  byMeFeedbackReducer
+  byMeFeedbackReducer,
+  shareFeedbackReducer
 });
